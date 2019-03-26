a7865dfd4809969cec5756b66ca5e89c799e926e
github username <github email address>
2019-03-26 15:05:26 -0500

    Update One

--- app/controllers/user_rewards_controller.rb
+++ app/controllers/user_rewards_controller.rb
@@ -6,19 +6,6 @@ class UserRewardsController < ApplicationController
 
     def show
         @user_reward = UserReward.find_by(user_id: @user.id)
-        total = 0
-        @user.donations.each do |donation|
-            total += donation.amount
-            if total <= 100
-                @user_reward.update(reward_id: 1)
-            elsif 100 < total <= 250
-                @user_reward.update(reward_id: 2)
-            elsif 250 < total <= 500
-                @user_reward.update(reward_id: 3)
-            elsif 500 < total
-                @user_reward.update(reward_id: 4)
-            end
-        end   
     end
 
     def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,72 +12,39 @@ Charity.destroy_all
 Reward.destroy_all
 UserReward.destroy_all
 
-# 30.times do 
-#     user = User.create({
-#       name: Faker::name.name,
-#       account_number: Faker::bank.account_number
-#       ammount_donated: Faker::number.within(1..1000)
-#     })
-# end
 
-User.create(name: "Name 1", account_number: 1234567890)
-User.create(name: "Name 2", account_number: 1236547890)
-User.create(name: "Name 3", account_number: 1234569870)
-User.create(name: "Name 4", account_number: 3214567890)
-  
-  
-# 30.times do
-#     donation = Donation.create({
-#       ammount: Faker::number.within(1..100),      
-#     })
-# end
+30.times do 
+    User.create(
+    name: Faker::Name.unique.name,
+    account_number: Faker::Bank.unique.account_number
+    )
+end
 
+10.times do
+    Charity.create(
+    name: Faker::Company.unique.name,
+    description: Faker::Company.industry    
+    )
+end
 
-Charity.create(name: "Charity 1", description: "description")
-Charity.create(name: "Charity 2", description: "description")
-Charity.create(name: "Charity 3", description: "description")
-Charity.create(name: "Charity 4", description: "description")
-Charity.create(name: "Charity 5", description: "description")
+100.times do
+    Donation.create(
+    user_id: User.all.sample.id,
+    charity_id: Charity.all.sample.id,
+    amount: Faker::Number.within(1..100),      
+    )
+end
 
-Reward.create(level: "bronze")
-Reward.create(level: "silver")
-Reward.create(level: "gold")
-Reward.create(level: "platinum")
-  
-UserReward.create(user_id: 1, reward_id: 1)
-UserReward.create(user_id: 2, reward_id: 2)
-UserReward.create(user_id: 3, reward_id: 3)
-UserReward.create(user_id: 4, reward_id: 4)
+Reward.create(level: "Bronze")
+Reward.create(level: "Silver")
+Reward.create(level: "Gold")
+Reward.create(level: "Platinum")
 
+def create_user_reward
+    User.all.each do |user|
+        UserReward.create(user_id: user.id, reward: Reward.find_by(level: "Bronze"))
+    end
+end
 
-Donation.create(user_id: 1, charity_id: 2, amount: 20)
-Donation.create(user_id: 1, charity_id: 1, amount: 20)
-Donation.create(user_id: 1, charity_id: 3, amount: 20)
-Donation.create(user_id: 1, charity_id: 4, amount: 20)
-Donation.create(user_id: 1, charity_id: 5, amount: 20)
-Donation.create(user_id: 2, charity_id: 1, amount: 20)
-Donation.create(user_id: 2, charity_id: 2, amount: 200)
-Donation.create(user_id: 2, charity_id: 3, amount: 20)
-Donation.create(user_id: 2, charity_id: 4, amount: 20)
-Donation.create(user_id: 2, charity_id: 5, amount: 20)
-Donation.create(user_id: 3, charity_id: 1, amount: 20)
-Donation.create(user_id: 3, charity_id: 2, amount: 20)
-Donation.create(user_id: 3, charity_id: 3, amount: 20)
-Donation.create(user_id: 3, charity_id: 4, amount: 450)
-Donation.create(user_id: 3, charity_id: 5, amount: 20)
-Donation.create(user_id: 4, charity_id: 1, amount: 20)
-Donation.create(user_id: 4, charity_id: 2, amount: 20)
-Donation.create(user_id: 4, charity_id: 3, amount: 20)
-Donation.create(user_id: 4, charity_id: 4, amount: 20)
-Donation.create(user_id: 4, charity_id: 5, amount: 1000)
-
-# 10.times do
-#     charity = Charity.create({
-#       name: Faker::name.initials
-#       description: Faker::lorem.paragraph    
-#     })
-# end
-
-
-
-# donation = donations.map { |donation| donation.merge( { user_id: User.all.sample.id, charity_id: Charity.all.sample.id } ) }
+create_user_reward
+puts "Hello"
