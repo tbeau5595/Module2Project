3c40a705f331bb0358e39aeb711137f8f8065afb
Theodore Kimana <kimana.theodore@gmail.com>
2019-03-28 17:56:53 -0500

    done

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -1,9 +1,13 @@
 class CharitiesController < ApplicationController
     def index
         @charities = Charity.all
+ 
     end
 
     def show
         @charity = Charity.find(params[:id])
     end
+    def new
+        @charity = "#{charity.name}"
+    end 
 end
