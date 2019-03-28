afe51fee83e08958d3889566a3c397bfe0299af0
github username <github email address>
2019-03-28 17:05:27 -0500

    added to donation#index

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -3,17 +3,26 @@ class DonationsController < ApplicationController
         @donation_amounts = Donation.all.map do |donation|
             donation.amount
         end
-        @largest_donation = @donation_amounts.max
+        @largest_donation_amount = @donation_amounts.max
+        
+        @largest_donation = Donation.find_by(amount: @largest_donation_amount)
+        @user_with_largest_donation = User.find(@largest_donation.user_id)
+        
         @charities = Charity.all
         
         @charities_donations = Donation.all.map do |donation|
             donation.charity_id
         end
-
         @donations_for_each_charity = Hash[@charities_donations.uniq.map {|i| [i, @charities_donations.count(i)]}]
-
         @most_donated_to = @donations_for_each_charity.values.max
         @most_donated_to_charity = Charity.find(@donations_for_each_charity.key(@most_donated_to))
+
+        @users_donations = Donation.all.map do |donation|
+            donation.user_id
+        end      
+        @donations_for_each_user = Hash[@users_donations.uniq.map {|i| [i, @users_donations.count(i)]}]
+        @most_donations = @donations_for_each_user.values.max
+        @user_with_most_donations = User.find(@donations_for_each_user.key(@most_donations))
     end
 
     def show
@@ -22,6 +31,8 @@ class DonationsController < ApplicationController
 
     def new
         @donation = Donation.new
+        @users = User.all
+        @charities = Charity.all
     end
 
     def edit
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,4 +2,6 @@ class Donation < ApplicationRecord
     belongs_to :user
     belongs_to :charity
     validates :amount, :numericality => { :greater_than_or_equal_to => 0 }
+
+    
 end
