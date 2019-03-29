053c5a66f267bc98c33814e968b3ce64d312f247
github username <github email address>
2019-03-29 10:25:09 -0500

    refactoring and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
     has_many :charities, through: :donations
     has_one :user_reward
     has_one :reward, through: :user_reward
+
+    validates :account_number, :uniqueness => true, :length => {is: 10}
+
     
 end
--- app/models/user_reward.rb
+++ app/models/user_reward.rb
@@ -1,6 +1,5 @@
 class UserReward < ApplicationRecord
     belongs_to :user
     belongs_to :reward
-
-    
+  
 end
